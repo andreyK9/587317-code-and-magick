a2784bfecaf3af418f06193590b2870370399f3d
andreyK9 <18041988@mail.ru>
2018-03-31 16:07:14 +0300

    отрисовал облако результатов

---
+++ js/stat.js
@@ -0,0 +1,7 @@
+window.renderStatistics = function(ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 10, 420, 270);
+}
