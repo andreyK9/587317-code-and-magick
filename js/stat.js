8fb3598759fa23080e66910930a1873551b0cd8b
andreyK9 <18041988@mail.ru>
2018-04-01 15:44:50 +0300

    сделал функцию вывода высоты перистого облака статистики

--- js/stat.js
+++ js/stat.js
@@ -11,21 +11,22 @@ var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var TEXT_COLOR = '#000';
 var MY_COLOR = 'rgba(255, 0, 0, 1)';
+var STEP_CUMULUS_CLOUD = 5;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
-var renderText = function(ctx, text, align , x, y) {
+var renderText = function (ctx, text, align, x, y) {
   ctx.fillStyle = TEXT_COLOR;
   ctx.textBaseline = 'top';
   ctx.textAlign = align;
   ctx.font = '16px PT Mono';
   ctx.fillText(text, x, y);
-}
+};
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -37,10 +38,28 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+var renderCumulusCloudHeight = function (ctx, beginX, beginY, step, height, color) {
+  var stepCurrent = Math.floor(CLOUD_HEIGHT / step);
+
+  ctx.beginPath();
+  ctx.moveTo(beginX, beginY);
+
+  for (var i = 0; i < step; i++) {
+    ctx.bezierCurveTo(beginX, beginY + stepCurrent * i, beginX + height, beginY + (stepCurrent / 2) + stepCurrent * i, beginX, beginY + stepCurrent * (i + 1));
+  }
+
+  ctx.closePath();
+  ctx.fillStyle = color;
+  ctx.fill();
+};
+
+window.renderStatistics = function (ctx, names, times) {
 
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  renderCumulusCloudHeight(ctx, CLOUD_X + CLOUD_WIDTH + GAP, CLOUD_Y + GAP, STEP_CUMULUS_CLOUD, BAR_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCumulusCloudHeight(ctx, CLOUD_X + CLOUD_WIDTH, CLOUD_Y, STEP_CUMULUS_CLOUD, BAR_GAP, '#fff');
 
   renderText(ctx, 'Ура вы победили!', 'center', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP);
   renderText(ctx, 'Список результатов:', 'center', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + FONT_GAP + GAP);
@@ -50,13 +69,13 @@ window.renderStatistics = function(ctx, names, times) {
   var minArrowLength = Math.min(names.length, times.length);
   names.length = times.length = minArrowLength;
 
-  for(var i = 0 ; i < names.length ; i++) {
+  for (var i = 0; i < names.length; i++) {
     renderText(ctx, names[i], 'left', CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - CLOUD_Y - GAP);
 
-    if(names[i] === 'Вы') {
+    if (names[i] === 'Вы') {
       ctx.fillStyle = MY_COLOR;
     } else {
-      var saturateControl  =  Math.round(Math.random() * 100);
+      var saturateControl = Math.round(Math.random() * 100);
       ctx.fillStyle = 'hsl(240, ' + saturateControl + '%, 50%)';
     }
 
@@ -67,4 +86,4 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.fillRect(CLOUD_X + BAR_GAP + currentBarStep, beginBarPosition - currentBarHeight, BAR_WIDTH, currentBarHeight);
     renderText(ctx, Math.round(times[i]), 'left', CLOUD_X + BAR_GAP + currentBarStep, beginBarPosition - FONT_GAP - currentBarHeight);
   }
-}
+};
